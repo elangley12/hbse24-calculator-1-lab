2ba233029abdb30012386b6d26fb9204d30ec538
Melissa McGrath <melissam640@gmail.com>
2024-02-28 12:18:04 -0800

    Complete arithmetic program

--- arithmetic.py
+++ arithmetic.py
@@ -4,32 +4,53 @@
 def add(num1, num2):
     """Return the sum of num1 and num2."""
 
-    return 10
+    answer = num1 + num2
+
+    return answer
 
 
 def subtract(num1, num2):
     """Return the value of num1 minus num2."""
 
+    answer = num1 - num2
+    return answer
+
+
 
 def multiply(num1, num2):
     """Multiply the num1 by num2 and return the result."""
 
+    answer = num1 * num2
+    return answer
 
 def divide(num1, num2):
     """Divide the num1 by num2, returning a floating point."""
+
+    answer = num1 / num2
+    return answer
 
 
 def square(num1):
     """Return the square of num1."""
 
+    answer = num1 * num1
+    return answer
 
 def cube(num1):
     """Return the cube of num1."""
+    answer = num1 * num1 * num1
+    return answer
 
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
 
+    answer = num1 ** num2
+    return answer
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
+
+    answer = num1 % num2
+    return answer
+
